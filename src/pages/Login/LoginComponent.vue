07163f0d8c0240b5a642c0cc377f55697616e017
Izaac Baptista <izaacbaptista@gmail.com>
2021-11-15 20:13:56 -0300

    create component login

--- src/pages/Login/LoginComponent.vue
+++ src/pages/Login/LoginComponent.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="login">
-        <h1>Login do nosso sistema</h1>
+    <div class="main">
+       <div class="login">
+           <div class="content-login">
+               <header>
+                   <h1>Login do Sistema</h1>
+               </header>
+           </div>
+       </div>
     </div>
 </template>
 
@@ -11,5 +17,41 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;700&display=swap');
 
+.main {
+    display: flex;
+    height: 100%;
+    flex-direction: row;
+    font-family: 'Roboto','sans-serif';
+}
+
+.login {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+}
+
+.content-login {
+    margin-top: 8%;
+    width: 400px;
+    height: 450px;
+    background: #82ACFA;
+    border-radius: 7px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+header {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+h1 {
+    color: white;
+}
 </style>
